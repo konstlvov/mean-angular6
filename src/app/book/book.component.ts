ff6f785af5b5457955dcdae6277dccdf349bfda6
Konstantin Lvov <k.lvov@abr.ru>
2018-08-13 12:01:23 +0300

    Добавлен класс BookDataSource и компоненты data source для таблицы Angular material

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-book',
@@ -9,9 +11,27 @@ import { ApiService } from '../api.service';
 export class BookComponent implements OnInit {
 
   books: any;
+  displayedColumns = ['isbn', 'title', 'author'];
+  dataSource = new BookDataSource(this.api);  
   constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.api.getBooks().subscribe(res => {console.log(res); this.books = res;}, err => {console.log(err);});
   }
 
 }
+
+export class BookDataSource extends DataSource<any> {
+  constructor(private api: ApiService) {
+    super();
+  }
+
+  connect() {
+    return this.api.getBooks();
+  }
+
+  disconnect() {
+  }
+
+}
+
